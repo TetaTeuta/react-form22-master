5065a18cdedf86aeeb97170ed404dc97f7ec57bd
TetaTeuta <teuta.stazic@gmail.com>
2020-07-19 16:30:39 +0200

    Updates

---
+++ precache-manifest.c1e38079e9d7608028e3d81d462ba966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36f7bb1308f702bcfe4b9a5e4f4edc6",
+    "url": "/react-form22-master/index.html"
+  },
+  {
+    "revision": "e9ebd8e92e09cabb6dd8",
+    "url": "/react-form22-master/static/css/main.cd5b628a.chunk.css"
+  },
+  {
+    "revision": "fc725f7939e326aa9a96",
+    "url": "/react-form22-master/static/js/2.427b7784.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-form22-master/static/js/2.427b7784.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9ebd8e92e09cabb6dd8",
+    "url": "/react-form22-master/static/js/main.3b62b34a.chunk.js"
+  },
+  {
+    "revision": "b7a8b19157c55d199800",
+    "url": "/react-form22-master/static/js/runtime-main.a109ca2c.js"
+  }
+]);
